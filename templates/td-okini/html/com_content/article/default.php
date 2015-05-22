71634f7fa6e10087f5d817c46acf4402baf4bf98
ophelie-mrnr <ophelie.marinier@etu.univ-tours.fr>
2015-05-22 15:52:01 +0200

    New BDD avec redirection accueil après inscription

--- templates/td-okini/html/com_content/article/default.php
+++ templates/td-okini/html/com_content/article/default.php
@@ -44,29 +44,14 @@ if ($params->get('show_title')) : ?>
 </hgroup>
 <?php endif; ?>
 
-<?php if ($params->get('access-edit') ||  $params->get('show_print_icon') || $params->get('show_email_icon')) : ?>
+<?php if ($params->get('access-edit')) : ?>
 		<ul class="actions">
 		<?php if (!$this->print) : ?>
-				<?php if ($params->get('show_print_icon')) : ?>
-				<li class="print-icon">
-						<?php echo JHtml::_('icon.print_popup', $this->item, $params, array(), true); ?>
-				</li>
-				<?php endif; ?>
-
-				<?php if ($params->get('show_email_icon')) : ?>
-				<li class="email-icon">
-						<?php echo JHtml::_('icon.email', $this->item, $params, array(), true); ?>
-				</li>
-				<?php endif; ?>
 				<?php if ($this->user->authorise('core.edit', 'com_content.article.'.$this->item->id)) : ?>
 						<li class="edit-icon">
 							<?php echo JHtml::_('icon.edit', $this->item, $params, array(), true); ?>
 						</li>
 					<?php endif; ?>
-		<?php else : ?>
-				<li>
-						<?php echo JHtml::_('icon.print_screen', $this->item, $params, array(), true); ?>
-				</li>
 		<?php endif; ?>
 		</ul>
 <?php endif; ?>
@@ -76,75 +61,6 @@ if ($params->get('show_title')) : ?>
 	endif; ?>
 
 	<?php echo $this->item->event->beforeDisplayContent; ?>
-
-<?php $useDefList = (($params->get('show_author')) or ($params->get('show_category')) or ($params->get('show_parent_category'))
-	or ($params->get('show_create_date')) or ($params->get('show_modify_date')) or ($params->get('show_publish_date'))
-	or ($params->get('show_hits'))); ?>
-
-<?php if ($useDefList) : ?>
- <dl class="article-info">
- <dt class="article-info-term"><?php  echo JText::_('COM_CONTENT_ARTICLE_INFO'); ?></dt>
-<?php endif; ?>
-<?php if ($params->get('show_parent_category') && $this->item->parent_slug != '1:root') : ?>
-		<dd class="parent-category-name">
-			<?php 	$title = $this->escape($this->item->parent_title);
-					$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->parent_slug)).'">'.$title.'</a>';?>
-			<?php if ($params->get('link_parent_category') and $this->item->parent_slug) : ?>
-				<?php echo JText::sprintf('COM_CONTENT_PARENT', $url); ?>
-				<?php else : ?>
-				<?php echo JText::sprintf('COM_CONTENT_PARENT', $title); ?>
-			<?php endif; ?>
-		</dd>
-<?php endif; ?>
-<?php if ($params->get('show_category')) : ?>
-		<dd class="category-name">
-			<?php 	$title = $this->escape($this->item->category_title);
-					$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)).'">'.$title.'</a>';?>
-			<?php if ($params->get('link_category') and $this->item->catslug) : ?>
-				<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $url); ?>
-				<?php else : ?>
-				<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $title); ?>
-			<?php endif; ?>
-		</dd>
-<?php endif; ?>
-<?php if ($params->get('show_create_date')) : ?>
-		<dd class="create">
-		<?php echo JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $this->item->created, JText::_('DATE_FORMAT_LC2'))); ?>
-		</dd>
-<?php endif; ?>
-<?php if ($params->get('show_modify_date')) : ?>
-		<dd class="modified">
-		<?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $this->item->modified, JText::_('DATE_FORMAT_LC2'))); ?>
-		</dd>
-<?php endif; ?>
-<?php if ($params->get('show_publish_date')) : ?>
-		<dd class="published">
-		<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $this->item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?>
-		</dd>
-<?php endif; ?>
-<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
-	<dd class="createdby">
-		<?php $author = $this->item->author; ?>
-		<?php $author = ($this->item->created_by_alias ? $this->item->created_by_alias : $author);?>
-
-			<?php if (!empty($this->item->contactid ) &&  $params->get('link_author') == true):?>
-				<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY',
-					JHtml::_('link', JRoute::_('index.php?option=com_contact&view=contact&id=' . $this->item->contactid), $author)
-				); ?>
-
-			<?php else :?>
-				<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
-			<?php endif; ?>
-	</dd>
-<?php endif; ?>
-<?php if ($params->get('show_hits')) : ?>
-		<dd class="hits">
-		<?php echo JText::sprintf('COM_CONTENT_ARTICLE_HITS', $this->item->hits); ?>
-		</dd>
-<?php endif; ?>
-<?php if ($useDefList) : ?>
- </dl>
-<?php endif; ?>
 
 	<?php if (isset ($this->item->toc)) : ?>
 		<?php echo $this->item->toc; ?>
